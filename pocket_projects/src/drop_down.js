23461924847d9ec86bf72eb76408cb52eee5ec2f
Hammad Khalid <hammaddotkhalid101@gmail.com>
2021-02-26 13:33:35 -0500

    Finsihed Phase 2 | added dropdown menu

--- pocket_projects/src/drop_down.js
+++ pocket_projects/src/drop_down.js
@@ -9,3 +9,48 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+const dogLinkCreate = (dogs) => {
+  let links = Object.values(dogs);
+  let names = Object.keys(dogs);
+  const doggies = [];
+  names.forEach( (dog, i) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.innerHTML = dog;
+    a.href = links[i];
+    li.className = 'dog-link';
+    li.appendChild(a);
+    doggies.push(li);
+  });
+
+  return doggies;
+} 
+
+const attachDogLinks = () => {
+  const dropDown = document.querySelector(".drop-down-dog-list");
+  const links = dogLinkCreate(dogs);
+
+  links.forEach(link => dropDown.appendChild(link));
+}
+attachDogLinks();
+
+const handleEnter = () => {
+  const links = document.querySelectorAll('.dog-link');
+
+  links.forEach(link => {
+    link.classList.add('show-link');
+  });
+}
+
+const handleLeave = () => {
+  const links = document.querySelectorAll('.dog-link');
+
+  links.forEach(link => {
+    link.classList.remove('show-link');
+  });
+}
+
+const dropDownNav = document.querySelector('.drop-down-dog-nav');
+
+dropDownNav.addEventListener('mouseenter', handleEnter);
+dropDownNav.addEventListener('mouseleave', handleLeave);
